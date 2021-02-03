42ad9dc61666d950a0d9f44c91ab2d3c6513e24f
Connor Detviler <connor.detviler@gmail.com>
2021-02-03 14:13:34 -0600

    created form and newItem object

--- src/components/ShelfForm/ShelfForm.jsx
+++ src/components/ShelfForm/ShelfForm.jsx
@@ -1,1 +1,44 @@
-import './ShelfForm.css'
+import './ShelfForm.css';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+
+
+function ShelfForm() {
+
+    const dispatch = useDispatch();
+
+    let [newItem, setNewItem] = useState({})
+
+    const handleChange = (event) => {
+        event.preventDefault();
+        const value = event.target.value;
+        setNewItem({...newItem, [event.target.name]: value})
+    }
+
+    const placeItem = () => {
+        dispatch({
+            type: 'ADD_ITEM',
+            payload: newItem
+        })
+    }
+
+    return (
+        <form onSubmit={placeItem}>
+            <input
+                type="text"
+                placeholder="description"
+                name="description"
+                onChange={handleChange}
+            />
+            <input
+                type="text"
+                placeholder="image url"
+                name="image_url"
+                onChange={handleChange}
+            />
+            <input type="submit" />
+        </form>
+    )
+}
+
+export default ShelfForm;
