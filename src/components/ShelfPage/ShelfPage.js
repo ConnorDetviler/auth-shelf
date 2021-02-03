ce4479819455db4ce0dabf27382f9471772ac591
Connor Detviler <connor.detviler@gmail.com>
2021-02-03 14:48:38 -0600

    conditional rendering on form

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import {React, useState} from 'react';
 import ShelfForm from '../ShelfForm/ShelfForm.jsx'
 
 function ShelfPage() {
+
+  let [formVis, setFormVis] = useState(false)
+
   return (
     <div className="container">
       <h2>Shelf</h2>
       <p>All of the available items can be seen here.</p>
-      <ShelfForm />
+
+      {!formVis && (
+        <button onClick={() => setFormVis(!formVis)}>Add Item!</button>
+      )}
+
+      {formVis && (
+          <>
+            <ShelfForm />
+            <button onClick={() => setFormVis(!formVis)}>Cancel</button>
+          </>
+      )}
     </div>
   );
 }
