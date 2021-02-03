3220e4babdb36dd3ae1cad5ff5f054cae6fdfdaa
Michael Dunn <dunnmj42@gmail.com>
2021-02-03 14:31:37 -0600

    shelf reducer in root and selector on shelfpage

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import ShelfItem from '../ShelfItem/ShelfItem';
 
 function ShelfPage() {
+
+  const dispatch = useDispatch();
+
+  const shelf = useSelector(store => store?.shelf)
+
+  console.log(shelf)
+
+  useEffect(() => {
+    dispatch({
+      type: 'FETCH_SHELF'
+    })
+  }, [])
+
   return (
     <div className="container">
       <h2>Shelf</h2>
       <p>All of the available items can be seen here.</p>
+      {shelf?.map((item) => (
+        <ShelfItem/>
+      ))}
     </div>
   );
 }
