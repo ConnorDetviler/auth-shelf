beec268505a0911c76b79ab7a49fc35db3324692
Michael Dunn <dunnmj42@gmail.com>
2021-02-03 14:17:58 -0600

    shelf get route and format

--- src/redux/reducers/shelf.reducer.js
+++ src/redux/reducers/shelf.reducer.js
@@ -1,7 +1,7 @@
 const shelfReducer = (state = [], action) => {
   switch (action.type) {
     case "SET_SHELF":
-      return action.payload
+      return action.payload;
     default:
       return state;
   }
--- src/redux/sagas/shelf.saga.js
+++ src/redux/sagas/shelf.saga.js
@@ -1,18 +1,17 @@
-import axios from 'axios';
-import { put, takeEvery } from 'redux-saga/effects';
-import shelfReducer from '../reducers/shelf.reducer';
+import axios from "axios";
+import { put, takeEvery } from "redux-saga/effects";
 
 function* fetchShelf() {
   try {
-     const response = yield axios.get("/api/shelf");
-     yield put({type: "SET_SHELF", payload: response.data})
+    const response = yield axios.get("/api/shelf");
+    yield put({ type: "SET_SHELF", payload: response.data });
   } catch (error) {
     console.error(error);
   }
-};
+}
 
 function* shelfSaga() {
   yield takeEvery("FETCH_SHELF", fetchShelf);
-};
+}
 
 export default shelfSaga;
